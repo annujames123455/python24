1505358a5cc0671b9ce6878fba6e6215f871da4f
annujames123455 <annujames2028@cs.sjcetpalai.ac.in>
2024-10-01 15:13:17 +0530

    Add files via upload

---
+++ annu.py
@@ -0,0 +1,7 @@
+print("hello python")
+name=input("name of the student:")
+print(name)
+cgpa=float(input("ENTER YOUR CGPA"))
+print(cgpa)
+percentage_ mark=cgpa*10
+print("marks percentage",percentage_mark,"%")
